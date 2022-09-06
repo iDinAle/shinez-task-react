888121a5776fae15784ed5bdcd3587f8c33db5b0
Alexii Aidinov <alexiiaidinov@Aidinovs-MacBook-Pro.local>
2022-09-06 20:58:31 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useLayoutEffect } from 'react'
+import { Table } from 'antd'
+import 'antd/dist/antd.css'
+import AdsApi from './adsApi'
 
-function App() {
+const titleMap = {
+  'image_url': 'Image Url',
+  'ad_name': 'Ad name',
+  'total_revs': 'Revenues',
+  'total_spend': 'Spend',
+  'total_profit': 'Profit',
+  'total_sessions': 'Sessions',
+  'total_paid_clicks': 'Paid Clicks',
+  'total_page_views': 'Page Views',
+  'cpc': 'CPC',
+  'roas': 'ROAS',
+  'discrepancy': 'Discrepancy',
+  'prmp': 'PRPM',
+}
+
+const adKeys = Object.keys(titleMap)
+
+const getColumns = () => (
+  adKeys
+    .filter(field => adKeys.includes(field))
+    .map(field => ({
+      title: titleMap[field],
+      dataIndex: field,
+      key: field,
+      render: (_text, record) => {
+        adKeys.forEach(key => {
+          switch (key) {
+            case 'total_revs':
+            case 'total_spend':
+            case 'total_profit':
+            case 'cpc':
+              record[key] = `${parseFloat(record[key]).toFixed(2)}$`
+              break
+            case 'roas':
+              record[key] = `${parseFloat(record[key]).toFixed(2)}%`
+              break
+            case 'discrepancy':
+              record[key] = record['total_paid_clicks'] 
+                ? `${parseFloat(record['total_sessions'] / record['total_paid_clicks'] * 100).toFixed(2)}%`
+                : `${parseFloat(0).toFixed(2)}%`
+              break
+            case 'prmp':
+              record[key] = `${parseFloat(1000 * parseFloat(record['total_revs']) / record['total_page_views']).toFixed(2)}$`
+              break
+            case 'image_url':
+              record[key] = record[key] || ''
+              break
+            default:
+              break
+          }    
+        })
+
+        return (
+          <>
+            {field === 'image_url'
+              ? <>{record[field] ? <img src={record[field]}></img> : <p></p>}</>
+              : <p>{record[field]}</p>}
+          </>
+        )
+      }
+    }))
+)
+
+const App = () => {
+  const [ads, setAds] = useState([])
+
+  useLayoutEffect(() => {
+    const fetchData = async () => {
+      const { data } = await AdsApi.getAds()
+
+      setAds(data)
+
+      console.log(getColumns(ads));
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table dataSource={ads} columns={getColumns()} />
     </div>
   );
-}
+};
 
 export default App;
